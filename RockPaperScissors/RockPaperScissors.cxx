792821376acd33c764732096b6d8429e8722680f
joker <joker@joker>
2014-08-26 21:05:59 +0200

    RockPaperScissors: Added chunk of symmetric peer-to-peer TCP network connection.
    A symmetric connection if some kind of peer-to-peer connection where no mediator server is needed for the peers to find each other. Instead, the peers hostname and port number is explicitly specified, and they are searching for each other on the net with trying to connect to and accept from the same place.
    The current code chunk contains a basin session class which resolves the remote hostname and then calls an empty method chunk that will be filled with the sysmmetric connection establishment code.
    The main program tries the whole thing out by resolving the remote host given as command line argument. Command line also requires the port number for "near future use".

--- RockPaperScissors/RockPaperScissors.cxx
+++ RockPaperScissors/RockPaperScissors.cxx
@@ -1,6 +1,90 @@
+#include <string>
+#include <iostream>
+
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+
 #include "RockPaperScissorsConfig.hxx"
 
-int main ()
+
+namespace p2p
 {
-  return 1;
+  using namespace boost::asio;
+
+  /**
+   * Abstract session base class.
+   */
+  class Session
+  {
+  public:
+    Session (io_service& ioService, const std::string& host, short port)
+      : mIoService (ioService)
+      , mResolver (ioService)
+      , mSocket (ioService)
+      , mHost (host)
+      , mPort (port)
+    {
+      // do nothing
+    }
+
+    ~Session ()
+    {
+      if (mSocket.is_open())
+      {
+        mSocket.close ();
+      }
+    }
+
+    void run ()
+    {
+      mResolver.async_resolve (ip::tcp::resolver::query (ip::tcp::v4 (), mHost, std::to_string (mPort)), [&](const boost::system::error_code& error, const ip::tcp::resolver::iterator& iter)
+      {
+        std::cout << "resolve with error: " << error << std::endl;
+        if (!error && iter != ip::tcp::resolver::iterator ())
+        {
+          connectAndListen (*iter);
+        }
+      });
+    }
+
+    void connectAndListen (const ip::tcp::endpoint& endpoint)
+    {
+      std::cout << "Connect and listen to " << endpoint << std::endl;
+    }
+
+  private:
+    io_service& mIoService;
+    ip::tcp::resolver mResolver;
+    ip::tcp::socket mSocket;
+
+    const std::string& mHost;
+    short mPort;
+  };
 }
+
+
+int main (int argc, char** argv)
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cout << "Usage: RockPaperScissors HOST PORT" << std::endl;
+      return 1;
+    }
+
+    boost::asio::io_service ioService;
+    p2p::Session session (ioService, argv[1], std::stoi (argv[2]));
+    session.run ();
+    std::cout << "Starting game..." << std::endl;
+    ioService.run ();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  std::cout << "Game ended." << std::endl;
+  return 0;
+}
+
